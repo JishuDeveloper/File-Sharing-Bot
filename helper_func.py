d05dbd5e2cebccfca22033adf605d78417c9bd3c
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-03-17 02:07:59 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -59,19 +59,15 @@ async def is_subscribed(filter, client, update):
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
     except UserNotParticipant:
         return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    
     
     try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
     except UserNotParticipant:
         return False
         
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    
+    else:
+        return True
+        
         
 async def encode(string):
     string_bytes = string.encode("ascii")
