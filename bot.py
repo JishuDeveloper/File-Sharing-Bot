22ca98a373049a34dd40e567ee45d656f48b8722
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-03-26 00:16:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,6 +93,20 @@ class Bot(Client):
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
                 sys.exit()
 
+        if FORCE_SUB_CHANNEL5:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL5)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL5)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL5)).invite_link
+                self.invitelink4 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL5 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL5}")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support for support")
+                sys.exit()     
+                
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
