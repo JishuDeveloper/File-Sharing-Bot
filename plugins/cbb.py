b9cdb2d1cb16518cfc974013b31401d2d275be01
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2024-06-12 15:34:55 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={1308086294}'>ᴍɪɴᴀᴛᴏ-ꜱᴀᴍᴀ</a>\n○ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/IAS_MOVIESANDSERIES'>Channel</a>\n○ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ : <a href='https://t.me/IAS_MOVIE_CHATS'>Chat</a>\n○ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/+6LZSdAbBv4c0MDVl'>Channel</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
