c3b73b009739a05bfd452ec47f3c9091956c32ca
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2024-06-12 15:52:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,7 +59,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "Nigha! You Ain't My Owner!!\n\n» My Owner : @Minato2op""
+USER_REPLY_TEXT = "Nigha! You Ain't My Owner!!\n\n» My Owner : @Minato2op"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1308086294)
