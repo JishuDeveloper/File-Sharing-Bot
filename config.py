8cac681edce8731783bacfacc7a930b5b779c6f5
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2024-06-13 17:56:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get("API_HASH", "b6866140cf6064d6c27de44915015879")
 
 OWNER = os.environ.get("OWNER", "@Minato2op") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "1308086294")) #Owner user id
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://hshhentai:hshhentai@cluster0.vhi4kjp.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001766230079"))
