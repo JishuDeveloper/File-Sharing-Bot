32c90ff82342731754ef044b2fd4622362438a4f
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2024-06-14 10:13:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,12 +92,7 @@ async def start_command(client: Client, message: Message):
                 f=await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis File Will Be Deleted In 15 minutes (Due To Copyright Issues).\n\n📌 Please Forward This File To Your Saved Messages Or Somewhere Else And Start Downloading There.")
-        await asyncio.sleep(SECONDS)
-        await f.delete()
-        await k.edit_text("Your File is Successfully Deleted !")
-
-
+        
         return
     else:
         reply_markup = InlineKeyboardMarkup(
