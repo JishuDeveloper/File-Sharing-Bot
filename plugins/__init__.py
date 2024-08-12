4039139fd1e176f8f8c2de01abf023f9ccf25d4a
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-12 10:21:16 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,3 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
-
-
 from aiohttp import web
 from .route import routes
 
