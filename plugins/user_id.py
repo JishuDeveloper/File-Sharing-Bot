55554d98483ac5ff6ed6a515b17a66c379a6ef5a
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-12 09:18:45 +0530

    Create user_id.py

---
+++ plugins/user_id.py
@@ -0,0 +1,16 @@
+from pyrogram import filters, enums
+from pyrogram.types import Message
+
+from bot import Bot
+
+@Bot.on_message(filters.command("id") & filters.private)
+async def showid(client, message):
+    chat_type = message.chat.type
+
+    if chat_type == enums.ChatType.PRIVATE:
+        user_id = message.chat.id
+        await message.reply_text(
+            f"<b>Your User ID Is :</b> <code>{user_id}</code>", 
+            quote=True
+        )
+        
