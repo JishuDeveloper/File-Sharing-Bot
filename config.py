32cf3678f2ec47d8ad387e232ff860e7d83f71d8
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-12 09:20:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
-SECONDS = int(os.getenv("SECONDS", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
 
 
 
