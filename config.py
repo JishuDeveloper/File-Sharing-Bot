2ef72a298c36b0fc999985b476e911ac247a4efe
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-12 12:26:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
